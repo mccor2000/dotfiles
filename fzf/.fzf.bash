9b785cb4a7e9e678f0fc0dc6cdb7cba4e5277894
mccor2000 <roccm0200@gmail.com>
2021-02-26 20:16:53 +0700

    add fzf

---
+++ fzf/.fzf.bash
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/mccor/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/mccor/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/mccor/.fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/mccor/.fzf/shell/key-bindings.bash"
